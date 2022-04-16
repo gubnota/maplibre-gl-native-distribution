16421628de093c5ec727cf4c3b1bbfa8fca8176c
gubnota <hvw@yandex.com>
2022-04-16 10:06:56 +0800

    init

---
+++ Package.swift
@@ -0,0 +1,19 @@
+// swift-tools-version:5.3
+import PackageDescription
+
+let package = Package(
+    name: "MapLibre GL Native",
+    products: [
+        .library(
+            name: "Mapbox",
+            targets: ["Mapbox"])
+    ],
+    dependencies: [
+    ],    
+    targets: [
+        .binaryTarget(
+            name: "Mapbox",
+            url: "https://gitee.com/gubnota/maplibre-gl-native-distribution/repository/archive/ios-v5.12.2.zip"
+            )
+    ]
+)
